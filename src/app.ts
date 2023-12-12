6d40fc8d36ad55c46134d09b062d0a20a2bd21a1
Shakil53 <shakilahmed.dcc@gmail.com>
2023-12-12 22:40:36 +0600

    courseRoutes and categoryRoutes refactories

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import { CourseRoutes } from './app/modules/course/course.route'
+import { CategoryRoutes } from './app/modules/category/category.route'
+import { ReviewRoutes } from './app/modules/review/review.route'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
 
 const app: Application = express()
 
@@ -13,10 +16,14 @@ app.use(cors())
 
 //application route
 app.use('/api/v1', CourseRoutes)
+app.use('/api',CategoryRoutes)
+app.use('/api',ReviewRoutes)
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
+
+app.use(globalErrorHandler);
 
 // console.log(process.cwd());
 
